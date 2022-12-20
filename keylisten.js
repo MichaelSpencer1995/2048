a4dee7a4f98a0ac1e339f6e02f4a1d3ef63c8e0c
Michael Spencer <michael.spencer1@g.austincc.edu>
2022-12-20 15:30:16 -0600

    add touch listeners for mobile

--- keylisten.js
+++ keylisten.js
@@ -7,6 +7,51 @@ document.onkeydown = function(e) {
     }
 }
 
+document.addEventListener('touchstart', handleTouchStart, false)
+document.addEventListener('touchmove', handleTouchMove, false)
+
+let xDown = null
+let yDown = null
+
+function getTouches(evt) {
+    return evt.touches
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0]
+    xDown = firstTouch.clientX
+    yDown = firstTouch.clientY
+}
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return
+    }
+
+    let xUp = evt.touches[0].clientX
+    let yUp = evt.touches[0].clientY
+
+    let xDiff = xDown - xUp
+    let yDiff = yDown - yUp
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+        if ( xDiff > 0 ) {
+            main('ArrowRight')
+        } else {
+            main('ArrowLeft')
+        }
+    } else {
+        if ( yDiff > 0 ) {
+            main('ArrowDowm')
+        } else { 
+            main('ArrowUp')
+        }
+    }
+
+    xDown = null
+    yDown = null
+}
+
 const restartButtons = document.getElementsByClassName('restart-button')
 const continueGameButton = document.querySelector('.keep-playing-button')
 
